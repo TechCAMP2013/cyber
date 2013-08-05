3d7279d98bf5116e7148776f5b4dfa5a2ee1faad
proshunsuke <shunsuke0901@gmail.com>
2013-08-06 00:55:18 +0900

    doya

--- src/ya/example/manga4/MainActivity.java
+++ src/ya/example/manga4/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 
 	//���������������������
 	//いいかげんにしろよ
+	// haihai
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
